ba23c96decdfd78dcd8134e20ec6f158c2e70e7b
pleffler@vols.utk.edu <pleffler@vols.utk.edu>
2018-05-13 12:12:01 -0500

    added cloudinary image conversion

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const cloudinary = require('cloudinary')
+const bodyParser = require("body-parser");
+
 const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 require('dotenv').config()
 
 cloudinary.config({ 
@@ -11,7 +15,11 @@ cloudinary.config({
 
 app.post('/jpgtopng', (req, res) => {
   // blah blah convert image
-  res.send('Image Converter API')
+  cloudinary.v2.uploader.upload(req.body.url, 
+    (error, result) => {
+      res.send(cloudinary.image(result.public_id, { format: 'png' }))
+    }
+  );
 })
 
 app.listen(3000, () => console.log('Image Converter API running on port 3000!'))
